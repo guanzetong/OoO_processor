2e18681c5a3a9e188fa1d3358ab078640d4a7f19
xiongrob <62448777+xiongrob@users.noreply.github.com>
2022-03-06 12:17:41 -0500

    Finish declaration of signals for ROB.v.

--- verilog/ROB.sv
+++ verilog/ROB.sv
@@ -26,52 +26,61 @@
 
 // Grouping of signals going to ( and from ) a specific module?
 typedef struct packed {
-    logic   [`DISPATCH_WIDTH-1:0]                               dispatch_en_i,
-    logic   [`ARCH_REG_IDX_WIDTH*`DISPATCH_WIDTH-1:0]     dispatch_arch_reg_i,
-    logic   [`TAG_WIDTH*`DISPATCH_WIDTH-1:0]                   dispatch_tag_i,
-    logic   [`TAG_WIDTH*`DISPATCH_WIDTH-1:0]               dispatch_tag_old_i,
-    logic   [`DISPATCH_WIDTH-1:0]                       dispatch_br_predict_i
+    logic   [`DISPATCH_WIDTH-1:0]                       dispatch_en_i           ,
+    logic   [`ARCH_REG_IDX_WIDTH*`DISPATCH_WIDTH-1:0]   dispatch_arch_reg_i     ,
+    logic   [`TAG_WIDTH*`DISPATCH_WIDTH-1:0]            dispatch_tag_i          ,
+    logic   [`TAG_WIDTH*`DISPATCH_WIDTH-1:0]            dispatch_tag_old_i      ,
+    logic   [`DISPATCH_WIDTH-1:0]                       dispatch_br_predict_i   
 } DISPATCH_ROB;
 
 typedef struct packed {
-    
-} FREE_LIST_IN;
+    logic  update_en;
+
+} ROB_ARCH_MAP_TABLE; // 
 
 typedef struct packed {
-    logic  
-} ROB_MAP_TABLE; // 
+    bit valid;
+    bit [`TAG_IDX_WIDTH] arch_reg;
+    bit [`TAG_IDX_WIDTH] phy_reg;
+} MAP_TABLE_ENTRY;
 
 typedef struct packed {
-} ROB_FREE_LIST; // 
+    bit valid;
+    bit [`TAG_IDX_WIDTH] phy_reg;
+} FREED_REG;
+// ROB_FREE_LIST rob_free.data[ 0 ].valid;
 
 typedef struct packed{
-    logic cdb_valid,                     // Is this signal valid?
-    logic [`TAG_IDX_WIDTH-1:0] cdb_tag,  // Physical Register
-    logic [`ROB_IDX_WIDTH-1:0] rob_index // Used to locate rob entry
+    logic                       cdb_valid   ,   // Is this signal valid?
+    logic [`TAG_IDX_WIDTH-1:0]  cdb_tag     ,   // Physical Register
+    logic [`ROB_IDX_WIDTH-1:0]  rob_index       // Used to locate rob entry
 } CDB_PACKET;
 
 typedef struct packed {
-    logic [`XLEN-1:0] PC, 
-    logic [`XLEN-1:0] NPC, 
+    // logic [`XLEN-1:0] PC, 
+    // logic [`XLEN-1:0] NPC, 
     logic [`TAG_IDX_WIDTH-1:0] T_old,
     logic [`TAG_IDX_WIDTH-1:0] T_new,
-    ALU_FUNC alu_func,
-    ALU_OPA_SELECT sel_a,
-    ALU_OPB_SELECT sel_b,
-
+ 
     logic [`ROB_IDX_WIDTH-1:0] arch_reg,
     logic           complete,
-    tag
 } ROB_ENTRY; // 
+
+typedef struct packed {
+    logic branch_valid,
+    logic branch_result,
+    logic branch_rob_num,
+} branch_rob;
+
 
 module ROB # 
     ( 
-    parameter   C_DISPATCH_WIDTH    =   `C_DISPATCH_WIDTH_M ,
-    parameter   C_CDB_WIDTH         =   `C_CDB_WIDTH_M      ,
-    parameter   C_RETIRE_WIDTH      =   `C_RETIRE_WIDTH_M   ,
-    parameter   C_ROB_ENTRY_NUM     =   `C_ROB_ENTRY_NUM_M  ,
-    parameter   C_ARCH_REG_NUM      =   `C_ARCH_REG_NUM_M   ,
-    parameter   C_PHY_REG_NUM       =   `C_PHY_REG_NUM_M    
+    parameter   C_DISPATCH_WIDTH    =   `DISPATCH_WIDTH   ,
+    parameter   C_CDB_WIDTH         =   `CDB_WIDTH        ,
+    parameter   C_RETIRE_WIDTH      =   `RETIRE_WIDTH     ,
+    parameter   C_ROB_ENTRY_NUM     =   `ROB_ENTRY_NUM    ,
+    parameter   C_ARCH_REG_NUM      =   `ARCH_REG_NUM     ,
+    parameter   C_PHY_REG_NUM       =   `PHY_REG_NUM      
     ) 
     (
     input                              clk_i, rst_i, // Clock and reset respectively
@@ -79,8 +88,12 @@ module ROB #
     input  [ C_DISPATCH_WIDTH ] dispatch_arch_reg_i,
     input                            dispatch_tag_i,
     input                        dispatch_tag_old_i,
+    input                        precise_state_en_i,
+    input                            branch_valid_i,
 
     output         [ C_DISPATCH_WIDTH ] rob_ready_o
+    output   [C_RETIRE_WIDTH-1:0] FREED_REG phy_reg_freed_o,
+    output   [C_RETIRE_WIDTH-1:0] MAP_TABLE_ENTRY arch_map_entry_update_o,
     );
 
 endmodule
