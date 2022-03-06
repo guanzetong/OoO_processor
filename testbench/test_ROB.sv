613691d0b5fcb32211abce7cffbef5aab8ca8d84
xiongrob <62448777+xiongrob@users.noreply.github.com>
2022-03-06 11:08:19 -0500

    Add test_ROB.sv.

---
+++ testbench/test_ROB.sv
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+//  Modulename  :  test_ROB.sv                                         //
+//                                                                     //
+//  Description :  Test ROB MODULE of the pipeline;                    // 
+//                 Reorders out of order instructions                  //
+//                 and update state (as if) in the archiectural        //
+//                 order.                                              //
+//                                                                     //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+
+/*
+1. Dispatch, fill all entries
+2. Complete,
+3. Retire, empty all entries,
+4. Dispatch, Complete some, retire some
+*/
