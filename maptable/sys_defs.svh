32cd50d00d9d6be8a989cef69e1d48317627eb2d
Ziyi Zhang <stzzzy@umich.edu>
2022-03-24 23:34:56 -0400

    map table testbench, issue with same rd and rs

--- maptable/sys_defs.svh
+++ maptable/sys_defs.svh
@@ -1,9 +1,13 @@
-`define MT_ENTRY 64
+`define MT_ENTRY 32
 `define ARCH_REG_NUM    32  // The number of Architectural registers.
-`define PHY_REG_IDX $clog2(PHY_REG_NUM)
 `define PHY_REG_NUM     64  // The number of Physical registers.
+`define ROB_IDX_WIDTH 5
+`define DP_NUM 2
+`define CDB_NUM 2
+`define PHY_REG_IDX     $clog2(`PHY_REG_NUM)
 `define ARCH_REG_IDX_WIDTH  $clog2(`ARCH_REG_NUM)
 `define TAG_IDX_WIDTH       $clog2(`PHY_REG_NUM)
+`define THREAD_IDX_WIDTH   $clog2(4)
 
 
 typedef struct packed {
@@ -12,12 +16,14 @@ typedef struct packed {
 } MP_ENTRY; // Per-channel
 
 typedef struct packed {
-    logic   [`ARCH_REG_IDX_WIDTH-1:0]               rs1         ;
-    logic   [`ARCH_REG_IDX_WIDTH-1:0]               rs2         ;
-    logic   [`ARCH_REG_IDX_WIDTH-1:0]               rd          ;
-    logic   [`TAG_IDX_WIDTH-1:0]                    tag         ;
-    logic                                           wr_en       ;
-    logic read_en;
+    logic   [`ARCH_REG_IDX_WIDTH-1:0]               rs1         ;   // used to index mt entry for source register 1
+    logic   [`ARCH_REG_IDX_WIDTH-1:0]               rs2         ;   // used to index mt entry for source register 2
+    logic                                           read_en;
+    logic   [`ARCH_REG_IDX_WIDTH-1:0]               rd          ;   // used to index mt entry for destination register
+    logic   [`TAG_IDX_WIDTH-1:0]                    tag         ;   // tag for destination register 
+    logic                                           wr_en       ;   // the corresponding mt entry ready for write
+    logic   [`THREAD_IDX_WIDTH-1:0]                 thread_idx  ;   // used for SMT 
+
 } DP_MT; // Per-Channel
 
 typedef struct packed {
@@ -37,5 +43,5 @@ typedef struct packed{
 } CDB; // Per-Channel
 
 typedef struct packed {
-    logic [`PHY_REG_IDX-1:0] rrat_tag;
+    logic [`PHY_REG_IDX-1:0] amt_tag;
 } AMT_ENTRY;
